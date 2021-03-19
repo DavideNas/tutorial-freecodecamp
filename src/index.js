ad68ea8d08ca90ac785c51a7e588b1567fc88a0c
DavideNas <d.naselli@gmail.com>
2021-03-19 18:06:04 +0100

    Key props and different read list method

--- src/index.js
+++ src/index.js
@@ -2,22 +2,30 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-// simple list
+// Iteration of Books list
 
 // delete fistBook and secondBook objects creating a list for both
 const books = [
   {
+    id: 1,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/71QuWEIWHlL._AC_UL200_SR200,200_.jpg",
     title: "Dog Man: Mothering Heights",
     author: "Dav Pilkey",
   },
-
   {
+    id: 2,
     author: "Amelia Hepworth",
     title: "I Love You to the Moon and Back",
     img:
       "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 3,
+    author: "Alyssa Satin Capucilli",
+    title: "Biscuit's Pet & Play Easter",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51gIBBYlqNL._AC_UL200_SR200,200_.jpg",
   },
 ];
 
@@ -25,13 +33,30 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book onebook={book} />;
+        return <Book key={book.id} onebook={book} />;
+        /* alternative for ID is:
+        books.map((book, index) =>
+        key={index}
+        but index change as array change */
+
+        /* alternative 1 to pass PARAMS is:
+        PARAMS pt 1
+        key={book.id} {...book} */
       })}
     </section>
   );
 }
 
 const Book = (props) => {
+  /*
+  alternative 1 PARAMS pt 2
+  const { img, title, author } = props; 
+  */
+
+  /*
+  alternative 2 PARAMS pt 2
+  const const Book = ({ img, title, author }) => {
+  */
   const { img, title, author } = props.onebook;
   return (
     <article className="book">
