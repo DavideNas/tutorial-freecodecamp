2a3eae51b0539471ee36bf559cd1999f9a48b692
DavideNas <d.naselli@gmail.com>
2021-03-19 13:04:23 +0100

    props variables

--- src/index.js
+++ src/index.js
@@ -2,32 +2,35 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-// Sample of Javascript in JSX using {} parenthesis
+// props
+
+const author = "Amelia Hepworth";
+const title = "I Love You to the Moon and Back";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg";
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book job="writer" />
+      <Book title="random title" number={22} />
     </section>
   );
 }
 
 // value out of component
-const author = "Amelia Hepworth";
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   // value inside component (it change visibility)
-  const title = "I Love You to the Moon and Back";
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-        alt=""
-      />
+      <img src={img} alt="" />
       <h1>{title}</h1>
-      {/* Possible add javascript into parenthesis */}
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{let x = 6}</p> */}
-      {6 + 6}
+      <h4>{author}</h4>
+      {/*props are printed only when are passed to component */}
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   );
 };
