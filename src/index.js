05c57f11d5e9f0f829fe6b5c762aed18062990a3
DavideNas <d.naselli@gmail.com>
2021-03-19 16:43:11 +0100

    Props - Children

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-// props destructured
+// props children
 
 const firstBook = {
   img:
@@ -25,7 +25,13 @@ function BookList() {
         img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
-      />
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro tempora
+          illo ipsum explicabo nulla. Accusantium nemo nostrum eos temporibus
+          nam.
+        </p>
+      </Book>
       <Book
         img={secondBook.img}
         title={secondBook.title}
@@ -35,15 +41,17 @@ function BookList() {
   );
 }
 
-// destructuring props , is auto-spread through variables
-const Book = ({ img, title, author }) => {
+const Book = ({ img, title, author, children }) => {
   // const { img, title, author } = props;  // must to be Book = (props)
-  // prev 2 lines make the same operation
+  // console.log(props);
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      {/* with children parameter It's possible to read paragraph <p> */}
+      {children}
+      {/* can modify css 'align-items' if i want fitted cards */}
     </article>
   );
 };
