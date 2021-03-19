6d2313b8e1a5d1201ce48a7d98618e206ae47fc8
DavideNas <d.naselli@gmail.com>
2021-03-19 17:14:03 +0100

    Simple List

--- src/index.js
+++ src/index.js
@@ -2,56 +2,44 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-// props children
+// simple list
 
-const firstBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/71QuWEIWHlL._AC_UL200_SR200,200_.jpg",
-  title: "Dog Man: Mothering Heights",
-  author: "Dav Pilkey",
-};
+// delete fistBook and secondBook objects creating a list for both
+const books = [
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71QuWEIWHlL._AC_UL200_SR200,200_.jpg",
+    title: "Dog Man: Mothering Heights",
+    author: "Dav Pilkey",
+  },
 
-const secondBook = {
-  author: "Amelia Hepworth",
-  title: "I Love You to the Moon and Back",
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-};
+  {
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  },
+];
 
+const ships = ["Nina", "Pinta", "Santa Maria"];
+// .map method get every element (ships) to single one (ship) and save to new object (newShips)
+const newShips = ships.map((ship) => {
+  // to render every element in <h1>
+  return <h1>{ship}</h1>;
+  //console.log(name);
+});
+console.log(newShips);
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro tempora
-          illo ipsum explicabo nulla. Accusantium nemo nostrum eos temporibus
-          nam.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  // printing newShips show me each 'ship' into <h1> tag
+  return <section className="booklist">{newShips}</section>;
 }
 
-const Book = ({ img, title, author, children }) => {
-  // const { img, title, author } = props;  // must to be Book = (props)
-  // console.log(props);
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {/* with children parameter It's possible to read paragraph <p> */}
-      {children}
-      {/* can modify css 'align-items' if i want fitted cards */}
     </article>
   );
 };
